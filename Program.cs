abc1e3ea5167b2c597f66722a05c71ecb5602e4c
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-03-06 17:20:01 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Pergunta5;
+
+class Program
+{
+    /*Escreva um programa que inverta os caracteres de um string.
+    IMPORTANTE:
+    a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+
+    b) Evite usar funções prontas, como, por exemplo, reverse;*/
+
+
+}
